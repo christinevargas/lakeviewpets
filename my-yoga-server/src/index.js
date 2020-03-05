50ac4f7e6d2d45ef52695de696614d24b1462f6f
Christine Vargas <christinevargas@Christines-MBP.localdomain>
2020-03-05 09:19:50 -0600

    Datamodel Creation Rough Draft

--- my-yoga-server/src/index.js
+++ my-yoga-server/src/index.js
@@ -1,84 +1,84 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 
-const resolvers = {
-    Query: {
-      posts: (_, args, context, info) => {
-        return context.prisma.query.posts(
-          {
-            where: {
-              OR: [
-                { title_contains: args.searchString },
-                { content_contains: args.searchString },
-              ],
-            },
-          },
-          info,
-        )
-      },
-      user: (_, args, context, info) => {
-        return context.prisma.query.user(
-          {
-            where: {
-              id: args.id,
-            },
-          },
-          info,
-        )
-      },
-    },
-    Mutation: {
-      createDraft: (_, args, context, info) => {
-        return context.prisma.mutation.createPost(
-          {
-            data: {
-              title: args.title,
-              content: args.content,
-              author: {
-                connect: {
-                  id: args.authorId,
-                },
-              },
-            },
-          },
-          info,
-        )
-      },
-      publish: (_, args, context, info) => {
-        return context.prisma.mutation.updatePost(
-          {
-            where: {
-              id: args.id,
-            },
-            data: {
-              published: true,
-            },
-          },
-          info,
-        )
-      },
-      deletePost: (_, args, context, info) => {
-        return context.prisma.mutation.deletePost(
-          {
-            where: {
-              id: args.id,
-            },
-          },
-          info,
-        )
-      },
-      signup: (_, args, context, info) => {
-        return context.prisma.mutation.createUser(
-          {
-            data: {
-              name: args.name,
-            },
-          },
-          info,
-        )
-      },
-    },
-  }
+// const resolvers = {
+//     Query: {
+//       posts: (_, args, context, info) => {
+//         return context.prisma.query.posts(
+//           {
+//             where: {
+//               OR: [
+//                 { title_contains: args.searchString },
+//                 { content_contains: args.searchString },
+//               ],
+//             },
+//           },
+//           info,
+//         )
+//       },
+//       user: (_, args, context, info) => {
+//         return context.prisma.query.user(
+//           {
+//             where: {
+//               id: args.id,
+//             },
+//           },
+//           info,
+//         )
+//       },
+//     },
+//     Mutation: {
+//       createDraft: (_, args, context, info) => {
+//         return context.prisma.mutation.createPost(
+//           {
+//             data: {
+//               title: args.title,
+//               content: args.content,
+//               author: {
+//                 connect: {
+//                   id: args.authorId,
+//                 },
+//               },
+//             },
+//           },
+//           info,
+//         )
+//       },
+//       publish: (_, args, context, info) => {
+//         return context.prisma.mutation.updatePost(
+//           {
+//             where: {
+//               id: args.id,
+//             },
+//             data: {
+//               published: true,
+//             },
+//           },
+//           info,
+//         )
+//       },
+//       deletePost: (_, args, context, info) => {
+//         return context.prisma.mutation.deletePost(
+//           {
+//             where: {
+//               id: args.id,
+//             },
+//           },
+//           info,
+//         )
+//       },
+//       signup: (_, args, context, info) => {
+//         return context.prisma.mutation.createUser(
+//           {
+//             data: {
+//               name: args.name,
+//             },
+//           },
+//           info,
+//         )
+//       },
+//     },
+//   }
 
 const server = new GraphQLServer({
   typeDefs: 'src/schema.graphql',
