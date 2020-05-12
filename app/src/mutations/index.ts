6871bde7af3e35508e43e568b58b9a609748e9af
Christine Vargas <christinevargas@christines-mbp.lan>
2020-05-12 16:45:29 -0500

    Removed all knex files as they are unnecessary

--- app/src/mutations/index.ts
+++ app/src/mutations/index.ts
@@ -1,5 +1,9 @@
 import CREATE_PET from './createPet';
 import UPDATE_PET from './updatePet';
 
+//TODO:
+//Create CREATE_USER
+//Create EDIT_USER
+
 
 export { CREATE_PET, UPDATE_PET  };
--- app/src/queries/index.ts
+++ app/src/queries/index.ts
@@ -2,4 +2,8 @@ import GET_ALL_PETS from "./getAllPets";
 import GET_PET from "./getPet";
 import GET_FILTERED_PETS from './getFilteredPets'
 
+//TODO:
+//Create GET_ALL_USERS
+//Create GET_USER
+
 export { GET_ALL_PETS, GET_PET, GET_FILTERED_PETS };
